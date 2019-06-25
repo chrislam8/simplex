114c263a7a0fa7fe53c5813d7f4f1a2bd85b9218
Christopher Lam <35906154+chrislam8@users.noreply.github.com>
2019-06-25 09:13:18 -0700

    more flexible testing code
    testing code allows for easy changing of the number of variables and number of constraints

--- test.cpp
+++ test.cpp
@@ -2,52 +2,58 @@
 #include <string>
 #include "simpl.h"
 
+#define NUMVAR 6
+#define NUMCONSTR 7
+#define NUMCOL 7
+#define NUMROW 8
+
 using namespace std;
 
 int main () {
-	simpl *test = new simpl(2,2);
+	simpl *test = new simpl(NUMVAR,NUMCONSTR);
 	bool isErr = false;
-	float** tableauCheck = new float*[3];
+	float** tableauCheck = new float*[NUMROW];
 	int i,j;
 	float testNum = -8.2;
-	for (i = 0; i < 3; i++) {
-		tableauCheck[i] = new float[3];
+	for (i = 0; i < NUMROW; i++) {
+		tableauCheck[i] = new float[NUMCOL];
 	}
-	test->changeValue(testNum,1,2);
-	if (!(test->changeValue(10.2,1,1))) {
-		cout << "Error: changeValue returned false when it should return true" << endl;
-		isErr = true;
+	//initialize values of tableau
+	for (i = 1; i <= NUMROW; i++) {
+		for (j = 1; j <= NUMCOL; j++) {
+			testNum = (i+j+1.0)/(i*j+1.0);
+			if (!(test->changeValue(testNum,i,j))) {
+				cout << "Error entering " << testNum << " into row " << i << " and " << j << endl;
+				isErr = true;
+			}
+		}
 	}
-	if (test->changeValue(10.2,10,1)) {
-		cout << "Error: changeValue returned true when it should return false" << endl;
-		isErr = true;
-	}
-	if (test->changeValue(10.2,0,1)) {
-		cout << "Error: changeValue returned true when it should return false" << endl;
-		isErr = true;
-	}
+	//looking at the values in the tableau
 	test->returnTableau(tableauCheck);
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
+	for (i = 0; i < NUMROW; i++) {
+		for (j = 0; j < NUMCOL; j++) {
 			cout << "Value in row " << (i+1) << " and column " << (j+1) << " is " << tableauCheck[i][j] << endl;
 		}
 	}
+	//pivot tableau
 	if (!(test->pivot(1,1))) {
 		cout << "Error: pivot returned false when it should return true" << endl;
 		isErr = true;
 	}
+	//looking at the values in the tableau after pivoting
 	test->returnTableau(tableauCheck);
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
+	for (i = 0; i < NUMROW; i++) {
+		for (j = 0; j < NUMCOL; j++) {
 			cout << "Value in row " << (i+1) << " and column " << (j+1) << " is " << tableauCheck[i][j] << endl;
 		}
 	}
 	if (!isErr) {
 		cout << "All tests successfully passed" << endl;
 	}
+	//free all dynamic memory used
 	delete test;
 	test = NULL;
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < NUMROW; i++) {
 		delete tableauCheck[i];
 		tableauCheck[i] = NULL;
 	}
