f4298134d9024718b9bc42a618e17403d7c9208c
Christopher Lam <chrislam.christopher@gmail.com>
2020-05-08 22:05:02 -0700

    Add output for success/fail of tests

--- simplex/main.cpp
+++ simplex/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+
 #include "test.h"
 
 int main() {
 	allTests testSeq;
-	testSeq.runAllTests();
+	testResultCodes testResult = testSeq.runAllTests();
+	if (testResult.second != TEST_SUCCESS)
+	{
+		std::cout << "Error: test failed";
+	}
+	else
+	{
+		std::cout << "All tests succeeded";
+	}
 	return 0;
 }
