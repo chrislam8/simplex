6530735a88a272881d5146c6ff34b6e0feb39e6e
Christopher Lam <cla318@sfu.ca>
2020-05-08 22:07:46 -0700

    Move tests into separate class

---
+++ simplex/test.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "testEnums.h"
+
+class allTests {
+public:
+	testResultCodes runAllTests();
+
+private:
+	testErrorCodes simplTest();
+	testErrorCodes negsizeTest();
+	testErrorCodes negrowTest();
+	testErrorCodes unboundedTest();
+	testErrorCodes infeasibleTest();
+	int NUMVAR = 2;
+	int NUMCONSTR = 3;
+	int NUMCOL = NUMVAR + 1;
+	int NUMROW = NUMCONSTR + 1;
+};
---
+++ simplex/testEnums.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <utility>
+
+enum testList {
+	SIMPL_TEST = 0,
+	NEG_SIZE_TEST,
+	NEGROW_TEST,
+	UNBOUNDED_TEST,
+	INFEASIBLE_TEST,
+
+	NUMBER_OF_TESTS
+};
+
+enum testErrorCodes {
+	TEST_SUCCESS,
+	TEST_INCORRECT_VALUE,
+	TEST_UNEXPECTED_VALUE,
+	TEST_INCORRECT_OPTIMAL_VALUE,
+	TEST_INCORRECT_OPERATION,
+	TEST_BLOCKED_CORRECT_OPERATION
+
+
+};
+
+#define testResultCodes std::pair<testList, testErrorCodes>
