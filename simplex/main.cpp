fb35112896b46ccff6e15893c40ebf614f17b35b
Christopher Lam <chrislam.christopher@gmail.com>
2020-12-21 21:17:04 -0800

    Add help text

--- simplex/main.cpp
+++ simplex/main.cpp
@@ -4,6 +4,7 @@
 #include "test.h"
 
 void runTests();
+void helpText();
 
 int main() 
 {
@@ -11,17 +12,32 @@ int main()
 	std::string input;
 	while (continuing)
 	{
+		bool realCommand = false;
 		std::cout << "What is your command?" << std::endl;
-		std::cout << "Command for exiting the program is 'exit'." << std::endl;
-		//std::cout << "Command for help on other commands is 'help'." << std::endl;
+		std::cout << "The command 'help' prints out the available commands." << std::endl;
 		std::getline(std::cin, input);
 		if (input == "exit")
 		{
 			continuing = false;
+			realCommand = true;
+		}
+		else
+		{
+			std::cout << std::endl;
 		}
 		if (input == "test")
 		{
 			runTests();
+			realCommand = true;
+		}
+		if (input == "help")
+		{
+			helpText();
+			realCommand = true;
+		}
+		if (!realCommand)
+		{
+			std::cout << input << " is not a real command. Try again.";
 		}
 		std::cout << std::endl << std::endl;
 	}
@@ -40,4 +56,12 @@ void runTests()
 	{
 		std::cout << "All tests succeeded";
 	}
+}
+
+void helpText()
+{
+	std::cout << "List of commands:" << std::endl << std::endl;
+	std::cout << "exit - exits the program" << std::endl;
+	std::cout << "help - prints the list of available commands" << std::endl;
+	std::cout << "test - runs all program tests";
 }
